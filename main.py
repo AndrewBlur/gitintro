b0994057083cb56ed0a0e77459a834e783951de3
[Andrew A] <[andrewgtc2005@gmail.com]>
2025-12-09 19:45:54 +0530

    Created a simple backend

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def main():
-    print("Hello from gitintro!")
+from fastapi import FastAPI
 
+app = FastAPI()
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+def hello_world():
+    return {"message": "Hello, World!"}
