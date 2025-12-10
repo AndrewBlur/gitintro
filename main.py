8e7752058b458294ff0709009f52bc05d65bf1a0
Andrew A <[andrewgtc2005@gmail.com]>
2025-12-10 13:47:02 +0530

    added a route to greet users

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 def hello_world():
     return {"message": "Hello, World!"}
+
+@app.get("/greet/{name}")
+def greet(name: str):
+    return {"message": f"Hello, {name}!"}
