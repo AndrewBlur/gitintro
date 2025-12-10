cb2bcdbbe56738a8816d5f7c56840040ef140ed1
Joey <[andrewgtc2005@gmail.com]>
2025-12-10 13:57:40 +0530

    Added frontend dashboard

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-@app.get("/")
-def hello_world():
-    return {"message": "Hello, World!"}
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/",response_class=HTMLResponse)
+def hello_world(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, "message": "Hello, World!"})
+
 
 @app.get("/greet/{name}")
 def greet(name: str):
